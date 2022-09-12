b1b313bc5d08e715a274b9d50f9e5c509eea9a46
Jyri Lindroos <jyri.lindroos@keuda.fi>
2022-09-12 11:29:18 +0300

    java-alkeet

---
+++ JS-tiedostot/javascript-perusteet.js
@@ -0,0 +1,32 @@
+function laskut()
+{
+  var luku1, luku2, summa, erotus, osamaara, tulo;
+  luku1 = 10;
+  luku2 = 2;
+  summa = luku1 + luku2;
+  erotus = luku1 - luku2;
+  osamaara = luku1 / luku2;
+  tulo = luku1 * luku2;
+  console.log("Summa = " + summa);
+  console.log("Erotus = " + erotus);
+  console.log("Osamäärä = " + osamaara);
+  console.log("Tulo = " + tulo);
+}
+function liitaSanat()
+{
+  var t1, t2, t3;
+  t1 = "Jyri ";
+  t2 = "on ";
+  t3 = "hyvä opettaja";
+  console.log(t1 + t1 + t3);
+}
+function attribuutti(nimi)
+{
+  var t1 = " on ";
+  var t2 = "hyvä opettaja";
+  console.log(nimi + t1 + t2);
+}
+function laskeYhteen(luku1, luku2)
+{
+  console.log(luku1 + luku2);
+}
